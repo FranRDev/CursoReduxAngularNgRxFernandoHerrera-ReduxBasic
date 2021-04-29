581cdfa15e950b1087b8c40756ef08289ea4f15c
Francisco Rodríguez <franciscorodriguezgar@gmail.com>
2021-04-29 16:54:33 +0200

    Clase 15. acciones y Reducer básico

--- app-1.ts
+++ app-1.ts
@@ -1,1 +1,18 @@
-console.log('Hola mundo');
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+function reducer(state = 10, action: Action) {
+    if (action.type === 'INCREMENTAR') {
+        return state += 1;
+    }
+
+    return state;
+}
+
+// Uso del "reducer".
+const incrementadorAction: Action = {
+    type: 'INCREMENTAR'
+};
+console.log(reducer(10, incrementadorAction));
