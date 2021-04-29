ed5e38928069edaa847391e7edddd6d44e853bf5
Francisco Rodríguez <franciscorodriguezgar@gmail.com>
2021-04-29 17:07:48 +0200

    Clase 16. Configurar más acciones en nuestro reducer

--- app-1.ts
+++ app-1.ts
@@ -4,11 +4,22 @@ interface Action {
 }
 
 function reducer(state = 10, action: Action) {
-    if (action.type === 'INCREMENTAR') {
-        return state += 1;
-    }
+    switch (action.type) {
+        case 'INCREMENTAR':
+            return state += 1;
 
-    return state;
+        case 'DECREMENTAR':
+            return state -= 1;
+
+        case 'MULTIPLICAR':
+            return state * action.payload;
+
+        case 'DIVIDIR':
+            return state / action.payload;
+
+        default:
+            return state;
+    }
 }
 
 // Uso del "reducer".
@@ -16,3 +27,21 @@ const incrementadorAction: Action = {
     type: 'INCREMENTAR'
 };
 console.log(reducer(10, incrementadorAction));
+
+const decrementadorAction: Action = {
+    type: 'DECREMENTAR'
+};
+console.log(reducer(10, decrementadorAction));
+
+const multiplicarAction: Action = {
+    type: 'MULTIPLICAR',
+    payload: 2
+};
+console.log(reducer(10, multiplicarAction));
+
+// Tarea: Dividir.
+const dividirAction: Action = {
+    type: 'DIVIDIR',
+    payload: 2
+};
+console.log(reducer(10, dividirAction));
